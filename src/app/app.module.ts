fa41047968526a357d2b1d1cc8e4a19c06cd2ac0
kamalarok <kamalarok29@gmail.com>
2023-06-07 19:46:40 +0530

    add deployment and development components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DevelopmentComponent } from './development/development.component';
+import { DeploymentComponent } from './deployment/deployment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DevelopmentComponent,
+    DeploymentComponent
   ],
   imports: [
     BrowserModule,
